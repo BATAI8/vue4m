e2143f00082992961c514f3eaeda9ec100b5d94f
BATAI8 <320235356@qq.com>
2020-04-03 13:09:46 +0800

    vscode提交代码

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -11,6 +11,8 @@
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
+        <!--  -->
+        <p>3weqweqweqqewew we</p>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"

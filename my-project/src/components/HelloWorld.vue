14f31038457d45758544dabc62def90adf77fe12
BATAI8 <320235356@qq.com>
2020-04-03 13:14:53 +0800

    提交

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
     <ul>
       <li>
         <!--  -->
-        <p>3weqweqweqqewewwe</p>
+        <p>3weqweqweqqe2313213wewwe</p>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
